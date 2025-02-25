c7451c6bc63ce9ecde9dfef061fefc7ce836b03c
Udhay Kumar <udhaya@convertcart.com>
2025-02-25 16:42:12 +0530

    Fix - data_version column of setup_module not getting updated

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,21 @@
+<?php
+namespace Vendor\Module\Setup;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+
+/* 
+    No data updates, 
+    but the presence of this file ensures 
+    Magento updates data_version of setup_module table
+*/
+
+class UpgradeData implements UpgradeDataInterface
+{
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        $setup->endSetup();
+    }
+}
