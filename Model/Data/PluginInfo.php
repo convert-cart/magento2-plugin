b2684c2abd3fa1907805b9eef3295629b9d88ea1
Udhay Kumar <udhaya@convertcart.com>
2025-03-07 17:02:40 +0530

    Plugin for fix

--- Model/Data/PluginInfo.php
+++ Model/Data/PluginInfo.php
@@ -9,36 +9,72 @@ class PluginInfo extends DataObject
     private $tables;
     private $triggers;
 
+    /**
+     * Get version
+     *
+     * @return string
+     */
     public function getVersion(): string
     {
         return $this->version;
     }
 
+    /**
+     * Set version
+     *
+     * @param string $version
+     * @return $this
+     */
     public function setVersion(string $version): void
     {
         $this->version = $version;
     }
 
+    /**
+     * Get tables
+     *
+     * @return array<string,bool>
+     */
     public function getTables(): array
     {
         return $this->tables;
     }
 
+    /**
+     * Set tables
+     *
+     * @param array<string,bool> $tables
+     * @return $this
+     */
     public function setTables(array $tables): void
     {
         $this->tables = $tables;
     }
 
+    /**
+     * Get triggers
+     *
+     * @return array<string,bool>
+     */
     public function getTriggers(): array
     {
         return $this->triggers;
     }
 
+    /**
+     * Set triggers
+     *
+     * @param array<string,bool> $triggers
+     * @return $this
+     */
     public function setTriggers(array $triggers): void
     {
         $this->triggers = $triggers;
     }
 
+    /**
+     * @return array
+     */
     public function jsonSerialize(): array
     {
         return [
