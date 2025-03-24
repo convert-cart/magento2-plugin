d10a4d8dcf4018a2de78e4c4d92273323d517b19
Mitanshu K <mitanshu@convertcart.com>
2025-03-24 11:35:44 +0530

    fix for magento 2.4.x

--- Plugin/ProductRepositoryPlugin.php
+++ Plugin/ProductRepositoryPlugin.php
@@ -4,67 +4,52 @@ namespace Convertcart\Analytics\Plugin;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\CatalogInventory\Api\StockItemRepositoryInterface;
+use Magento\CatalogInventory\Api\StockItemCriteriaInterface;
 use Magento\Catalog\Api\Data\ProductSearchResultsInterface;
-use Magento\Framework\Api\SearchCriteriaBuilder;
-use Psr\Log\LoggerInterface;
 
 class ProductRepositoryPlugin
 {
     protected $stockItemRepository;
-    protected $searchCriteriaBuilder;
-    protected $logger;
+    protected $stockItemCriteria;
 
     public function __construct(
         StockItemRepositoryInterface $stockItemRepository,
-        SearchCriteriaBuilder $searchCriteriaBuilder,
-        LoggerInterface $logger
+        StockItemCriteriaInterface $stockItemCriteria
     ) {
         $this->stockItemRepository = $stockItemRepository;
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->logger = $logger;
+        $this->stockItemCriteria = $stockItemCriteria;
     }
 
     public function afterGetList(ProductRepositoryInterface $subject, ProductSearchResultsInterface $searchResults)
     {
-        $this->logger->info('ProductRepositoryPlugin::afterGetList - Start processing');
-
         $productSkus = [];
 
         foreach ($searchResults->getItems() as $product) {
-            $productSkus[$product->getId()] = $product->getSku();
+            $productSkus[] = $product->getSku();
         }
 
-        $this->logger->info('Collected SKUs: ' . implode(',', $productSkus));
-
         if (!empty($productSkus)) {
-            try {
-                // Fetch stock data in bulk
-                $searchCriteria = $this->searchCriteriaBuilder->addFilter('sku', array_values($productSkus), 'in')->create();
-                $stockItems = $this->stockItemRepository->getList($searchCriteria)->getItems();
+            // Fetch stock items in bulk
+            $criteria = $this->stockItemCriteria->setSkus($productSkus);
+            $stockItems = $this->stockItemRepository->getList($criteria)->getItems();
 
-                $this->logger->info('Fetched stock items count: ' . count($stockItems));
+            // Map stock data to products
+            $stockData = [];
+            foreach ($stockItems as $stockItem) {
+                $stockData[$stockItem->getSku()] = $stockItem;
+            }
 
-                // Map stock data to products
-                foreach ($searchResults->getItems() as $product) {
-                    foreach ($stockItems as $stockItem) {
-                        if ($stockItem->getSku() === $product->getSku()) {
-                            $product->setData('stock_qty', $stockItem->getQty());
-                            $product->setData('is_in_stock', $stockItem->getIsInStock());
-                            $product->setData('manage_stock', $stockItem->getManageStock());
-                            $product->setData('backorders', $stockItem->getBackorders());
-
-                            $this->logger->info("Updated stock details for SKU: {$product->getSku()}");
-                            break;
-                        }
-                    }
+            foreach ($searchResults->getItems() as $product) {
+                if (isset($stockData[$product->getSku()])) {
+                    $stockItem = $stockData[$product->getSku()];
+                    $product->setData('stock_qty', $stockItem->getQty());
+                    $product->setData('is_in_stock', $stockItem->getIsInStock());
+                    $product->setData('manage_stock', $stockItem->getManageStock());
+                    $product->setData('backorders', $stockItem->getBackorders());
                 }
-            } catch (\Exception $e) {
-                $this->logger->error('Error in ProductRepositoryPlugin::afterGetList - ' . $e->getMessage());
             }
         }
 
-        $this->logger->info('ProductRepositoryPlugin::afterGetList - Processing completed');
-        
         return $searchResults;
     }
 }
