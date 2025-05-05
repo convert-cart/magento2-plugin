adbdb060067aee042c2e01983f5c843ff02bd6ae
Mitanshu K <mitanshu@convertcart.com>
2025-05-05 12:27:05 +0530

    fix: update UpgradeData.php file

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -6,17 +6,21 @@ use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 
-/* 
-    No data updates, 
-    but the presence of this file ensures 
-    Magento updates data_version of setup_module table
-*/
-
 class UpgradeData implements UpgradeDataInterface
 {
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
+
+        // Trigger on fresh install (when context->getVersion() is NULL)
+        if (!$context->getVersion()) {
+            $setup->getConnection()->query('SELECT 1');
+        }
+
+        // if (version_compare($context->getVersion(), '1.1.0', '<')) {
+        //     // Add upgrade logic for version 1.1.0 & above
+        // }
+
         $setup->endSetup();
     }
 }
