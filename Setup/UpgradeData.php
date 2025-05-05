9628d7e00357aa6cf27ee79e0ec076d7925d898a
Mitanshu K <mitanshu@convertcart.com>
2025-05-05 11:23:39 +0530

    updated upradeData file

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -1,35 +1,22 @@
 <?php
 
-namespace Convertcart\Analytics\Setup;
+namespace Vendor\Module\Setup;
 
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 
+/* 
+    No data updates, 
+    but the presence of this file ensures 
+    Magento updates data_version of setup_module table
+*/
+
 class UpgradeData implements UpgradeDataInterface
 {
-    /**
-     * Upgrade data for the module
-     *
-     * @param ModuleDataSetupInterface $setup
-     * @param ModuleContextInterface $context
-     */
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
-
-        if ($context->getVersion() && version_compare($context->getVersion(), '1.1.0', '<')) {
-            $setup->getConnection()->insert(
-                $setup->getTable('core_config_data'),
-                [
-                    'scope' => 'default',
-                    'scope_id' => 0,
-                    'path' => 'convertcart_analytics/new_setting',
-                    'value' => '1'
-                ]
-            );
-        }
-
         $setup->endSetup();
     }
 }
