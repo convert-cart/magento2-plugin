56e32bc716fd653661bfc9d7a15ef99e6a4498d3
Udhay Kumar <udhaya@convertcart.com>
2025-05-16 15:40:05 +0530

    Fixes for data version

---
+++ Setup/InstallData.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Convertcart\Analytics\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        // No data to install, but this ensures data_version is set in setup_module
+    }
+}
